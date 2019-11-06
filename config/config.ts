17645aa3ebc8c4ae0b791798f12c93c1ac9fb15b
张雨佳 <zhang@zhangyujiadeMacBook-Pro.local>
2019-11-06 21:19:53 +0800

    新建一个空白Page并加入路由

--- config/config.ts
+++ config/config.ts
@@ -79,49 +79,13 @@ export default {
   // umi routes: https://umijs.org/zh/guide/router.html
   routes: [
     {
-      path: '/user',
-      component: '../layouts/UserLayout',
-      routes: [
-        {
-          name: 'login',
-          path: '/user/login',
-          component: './user/login',
-        },
-      ],
-    },
-    {
       path: '/',
-      component: '../layouts/SecurityLayout',
+      component: '../layouts/BasicLayout',
       routes: [
         {
           path: '/',
-          component: '../layouts/BasicLayout',
-          authority: ['admin', 'user'],
-          routes: [
-            {
-              path: '/',
-              redirect: '/welcome',
-            },
-            {
-              path: '/welcome',
-              name: 'welcome',
-              icon: 'smile',
-              component: './Welcome',
-            },
-            {
-              path: '/admin',
-              name: 'admin',
-              icon: 'crown',
-              component: './Admin',
-              authority: ['admin'],
-            },
-            {
-              component: './404',
-            },
-          ],
-        },
-        {
-          component: './404',
+          name: '实时图表',
+          component: 'dashboard',
         },
       ],
     },
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+interface DashboardPageType {}
+
+const DashboardPage: FC<DashboardPageType> = () => {
+  const _ = 'F.U.C.K';
+  return (
+    <div>
+      <div>{_}</div>
+    </div>
+  );
+};
+
+export default DashboardPage;
